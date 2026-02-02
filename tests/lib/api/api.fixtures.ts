9b78da3cb3cc76e01e19426bd3fdd20f6b0b1bda
Syed Bilgrami <syed.bilgrami.ctr.in@dragonflyft.com>
2026-02-02 14:11:38 +0530

    feature/IEBAA_3716

--- tests/lib/api/api.fixtures.ts
+++ tests/lib/api/api.fixtures.ts
@@ -1,21 +1,25 @@
-// fixtures/api.fixtures.ts
 import { test as base } from '@playwright/test';
 import { UserApi } from './userApi';
 import { EntitlementApi } from './entitlementApi';
+import { UserIdAvailApi } from './userIdAvailApi';
 
 type ApiFixtures = {
   userApi: UserApi;
   entitlementApi: EntitlementApi;
+  userIdAvailApi: UserIdAvailApi;
 };
 
 export const test = base.extend<ApiFixtures>({
   userApi: async ({ request }, use) => {
-    const api = new UserApi(request);
-    await use(api);
+    await use(new UserApi(request));
   },
+
   entitlementApi: async ({ request }, use) => {
-    const api = new EntitlementApi(request);
-    await use(api);
+    await use(new EntitlementApi(request));
+  },
+
+  userIdAvailApi: async ({ request }, use) => {
+    await use(new UserIdAvailApi(request));
   },
 });
 
